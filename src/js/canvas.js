a5880e9e3865fc35361a3288f499fcc046a2e6ca
clement-m <clement.melchior@gmail.com>
2018-11-10 15:03:32 +0100

    lecture facile

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -15,13 +15,12 @@ window.onload = function()
     }
         
     var diametreBalle = 20;
-    
-    var posX = 1+diametreBalle/2;
-    var posY = 1+diametreBalle/2;
+    var posX = 1 + diametreBalle / 2;
+    var posY = 1 + diametreBalle / 2;
     var vitesseX = 3;
     var vitesseY = 3;
     
-    var myInterval = setInterval(animate, 1000/30);
+    var myInterval = setInterval(animate, 1000 / 30);
     
     function animate()
     {
@@ -29,16 +28,16 @@ window.onload = function()
         
         //Tracé de la balle
         context.beginPath();
-        context.arc(posX, posY, diametreBalle/2, 0, Math.PI*2);
+        context.arc(posX, posY, diametreBalle / 2, 0, Math.PI * 2);
         context.fill();
         
         //On va vérifier si la balle à toucher l'un des bords du canvas.
-        if(posX+diametreBalle/2 >= canvas.width || posX <= 0+diametreBalle/2)//Si on touche le bord gauche ou droit
+        if(posX + diametreBalle / 2 >= canvas.width || posX <= 0 + diametreBalle / 2)//Si on touche le bord gauche ou droit
         {
             vitesseX *= -1;//On inverse la vitesse de déplacement sur l'axe horizontal.
         }
 
-        if(posY+diametreBalle/2 >= canvas.height || posY <= 0+diametreBalle/2)//Si on touche le bord du bas ou du haut
+        if(posY + diametreBalle / 2 >= canvas.height || posY <= 0 + diametreBalle / 2)//Si on touche le bord du bas ou du haut
         {
             vitesseY *= -1;//On inverse la vitesse de déplacement sur l'axe vertical.
         }
