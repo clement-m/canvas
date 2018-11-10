aab3bbc2504e952aab8979d19037828bf5ded862
clement-m <clement.melchior@gmail.com>
2018-11-10 14:59:08 +0100

    balleProcedurale

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -13,34 +13,39 @@ window.onload = function()
         alert("Impossible de récupérer le context");
         return;
     }
-
-    var Ball1 = Object.create(Ball);
-
+        
+    var diametreBalle = 20;
+    
+    var posX = 1+diametreBalle/2;
+    var posY = 1+diametreBalle/2;
+    var vitesseX = 3;
+    var vitesseY = 3;
+    
     var myInterval = setInterval(animate, 1000/30);
     
     function animate()
     {
         context.clearRect(0, 0, canvas.width, canvas.height);
-
+        
         //Tracé de la balle
         context.beginPath();
-        context.arc(Ball1.posX, Ball1.posY, Ball1.diametreBall / 2, 0, Math.PI * 2);
+        context.arc(posX, posY, diametreBalle/2, 0, Math.PI*2);
         context.fill();
         
         //On va vérifier si la balle à toucher l'un des bords du canvas.
-        if(Ball1.posX + Ball1.diametreBall / 2 >= canvas.width || Ball1.posX <= 0 + Ball1.diametreBall / 2)//Si on touche le bord gauche ou droit
+        if(posX+diametreBalle/2 >= canvas.width || posX <= 0+diametreBalle/2)//Si on touche le bord gauche ou droit
         {
-            Ball1.speedX *= -1;//On inverse la vitesse de déplacement sur l'axe horizontal.
+            vitesseX *= -1;//On inverse la vitesse de déplacement sur l'axe horizontal.
         }
 
-        if(Ball1.posY + Ball1.diametreBall / 2 >= canvas.height || Ball1.posY <= 0 + Ball1.diametreBall / 2)//Si on touche le bord du bas ou du haut
+        if(posY+diametreBalle/2 >= canvas.height || posY <= 0+diametreBalle/2)//Si on touche le bord du bas ou du haut
         {
-            Ball1.speedY *= -1;//On inverse la vitesse de déplacement sur l'axe vertical.
+            vitesseY *= -1;//On inverse la vitesse de déplacement sur l'axe vertical.
         }
         
         //On additionne les vitesses de déplacement avec les positions
-        Ball1.posX += Ball1.speedX;
-        Ball1.posY += Ball1.speedY;
+        posX += vitesseX;
+        posY += vitesseY;
     }
     
 }
